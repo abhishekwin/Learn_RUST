3525589f336dfd54f91fe426724648017bae37a2
abhishek <akothiya06@gmail.com>
2024-11-30 18:19:12 +0530

    Understand Traits

---
+++ Generic_Type/src/traits.rs
@@ -0,0 +1,30 @@
+fn main() {
+    // Understanding Traits
+
+    let article = NewsArticle {
+        headline: String::from("Rust is awesome!"),
+        location: String::from("The Internet"),
+        author: String::from("John Doe"),
+        content: String::from("A lot of details about Rust..."),
+    };
+
+    println!("Summary: {}", article.summarize());
+}
+
+// Here we declare a trait with  "trait" keyword name and "Summary" trait.
+pub trait Summary {
+    fn summarize(&self) -> String;
+}
+
+// Here we implement the trait "Summary" for the struct "NewsArticle"
+pub struct NewsArticle {
+    pub headline: String,
+    pub location: String,
+    pub author: String,
+    pub content: String,
+}
+impl Summary for NewsArticle {
+    fn summarize(&self) -> String {
+        format!("{} by {} ({})", self.headline, self.author, self.location)
+    }
+}

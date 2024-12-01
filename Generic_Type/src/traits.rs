0fd04fced00dce5788bf3ca9d3fa5f9fe14c8bec
abhishek <akothiya06@gmail.com>
2024-12-01 17:43:45 +0530

    Learn Traits

--- Generic_Type/src/traits.rs
+++ Generic_Type/src/traits.rs
@@ -24,6 +24,8 @@ pub struct NewsArticle {
     pub content: String,
 }
 impl Summary for NewsArticle {
+
+    // Function
     fn summarize(&self) -> String {
         format!("{} by {} ({})", self.headline, self.author, self.location)
     }
